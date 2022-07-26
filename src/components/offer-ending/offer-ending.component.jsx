6303c0ffe3931f8145ccccbb8bbf814c43edfb2b
Rafiur Rahman <rrfoundary@gmail.com>
2022-07-26 17:04:23 +0600

    added offer ending component with style

--- src/components/offer-ending/offer-ending.component.jsx
+++ src/components/offer-ending/offer-ending.component.jsx
@@ -1,31 +1,37 @@
 import { useEffect, useState } from "react";
 import Service from "../../service/service";
+import "./offer-ending.style.scss";
+let timeInterval;
+function calculateTime() {
+  const millisecToSec = 1000;
+  const upcomingTime = Date.parse(new Date(Service.deal.time));
+  const currentTime = Date.parse(new Date());
+  const timeRemaining = (upcomingTime - currentTime) / 1000;
+  if (timeRemaining < 0) clearInterval(timeInterval);
+  const seconds = Math.floor(timeRemaining % 60);
+  const minutes = Math.floor((timeRemaining / 60) % 60);
+  const hours = Math.floor((timeRemaining / 60 / 60) % 24);
+  const days = Math.floor(timeRemaining / 60 / 60 / 24);
+  return { seconds, minutes, hours, days };
+}
 const OfferEnding = () => {
   const [time, setTime] = useState({});
-  let timeInterval;
+
   function timeLeft() {
-    const millisecToSec = 1000;
-    const upcomingTime = Date.parse(new Date(Service.deal.time));
-    const currentTime = Date.parse(new Date());
-    const timeRemaining = (upcomingTime - currentTime) / 1000;
-    if (timeRemaining < 0) clearInterval(timeInterval);
-    const seconds = Math.floor(timeRemaining % 60);
-    const minutes = Math.floor((timeRemaining / 60) % 60);
-    const hours = Math.floor((timeRemaining / 60 / 60) % 24);
-    const days = Math.floor(timeRemaining / 60 / 60 / 24);
-    setTime({ ...time, seconds, minutes, hours, days });
+    const updatedTime = calculateTime();
+    setTime({ ...time, ...updatedTime });
   }
   useEffect(() => {
     setInterval(timeLeft, 1000);
   }, []);
   return (
-    <div className="offer-ending">
+    <div className="offer-ending my-5">
       <div className="container">
         <div className="d-flex align-items-center">
           <img src={Service.deal.image} alt="" className="me-5" />
           <div className="offer-ending-content">
-            <h4 className="display-4">Deal of the day</h4>
-            <div className="offer-time d-flex">
+            <h4 className="display-4 text-center">Deal of the day</h4>
+            <div className="offer-time d-flex justify-content-between text-center my-4">
               <div>
                 <span className="d-block">{time.days}</span>
                 Days
@@ -43,7 +49,9 @@ const OfferEnding = () => {
                 Seconds
               </div>
             </div>
-            <button className="btn btn-secondary">Shop now</button>
+            <div className="text-center mt-4">
+              <button className="btn btn-secondary">Shop now</button>
+            </div>
           </div>
         </div>
       </div>
