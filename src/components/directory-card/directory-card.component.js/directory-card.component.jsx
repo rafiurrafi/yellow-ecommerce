9e177bdc15dd392084fa191be9513d00a6bd6f68
Rafiur Rahman <rrfoundary@gmail.com>
2022-08-14 20:07:52 +0600

    make directory component for women shop

--- src/components/directory-card/directory-card.component.js/directory-card.component.jsx
+++ src/components/directory-card/directory-card.component.js/directory-card.component.jsx
@@ -1,16 +1,28 @@
 import { MdOutlineArrowForwardIos } from "react-icons/md";
+import { Link } from "react-router-dom";
 import Assets from "../../../service/service";
 import "./directory-card.style.scss";
 const DirectoryCard = () => {
   return (
-    <div className="directory-card d-flex my-5">
+    <div className="directory-card d-md-flex my-5">
       <div className="directory-card-single">
         <img src={Assets.directoryCard[0]} alt="" />
         <div className="directory-card-content">
-          <h4 className="mb-0">Men</h4>
-          <button className="btn">
+          <h4 className="mb-0 fw-normal">Man Fashion Shop</h4>
+          <p>Choose Your Products Here</p>
+          <Link to="/shop" className="text-dark d-block float-end">
             <MdOutlineArrowForwardIos />
-          </button>
+          </Link>
+        </div>
+      </div>
+      <div className="directory-card-single">
+        <img src={Assets.directoryCard[1]} alt="" />
+        <div className="directory-card-content">
+          <h4 className="mb-0 fw-normal">Women Fashion Shop</h4>
+          <p>Choose Your Products Here</p>
+          <Link to="/shop" className="text-dark d-block float-end">
+            <MdOutlineArrowForwardIos />
+          </Link>
         </div>
       </div>
     </div>
